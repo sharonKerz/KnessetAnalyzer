2864b80c22bbad077a7640a8f54ec2727ff24da2
Guy Ben Moshe <gbt3101@gmail.com>
2016-07-10 15:09:27 +0300

    Basic working web added

---
+++ src/main/web/scripts/App.js
@@ -0,0 +1,29 @@
+'use strict';
+var app = angular.module("knessetAnalyzer", []); 
+app.controller("mainCtrl", function($scope, $http) {
+    $scope.dates = {startDate: new Date(99), endDate: new Date()};
+    //$scope.startDate = new Date(99);
+    //$scope.endDate = new Date();
+    $scope.subjectList = [];
+    $scope.sendingAddress = 'localhost';
+    $scope.received = false;
+    $scope.sent = false;
+    
+    $scope.submit = function() {
+        console.log("Submit Activated");
+        $scope.sent = true;
+        $http.post("http://"+$scope.sendingAddress+":3000/datesQuery", 
+                       $scope.dates)
+            .success(function(response) {
+                $scope.sent = false;
+                $scope.received = true;
+                $scope.subjectList = response;
+            });
+        // FOR DEBUG ONLY:
+        $scope.sent=false;
+        $scope.received = true;
+        $scope.subjectList = ['Security', 'Economy', 'Stupid Muslims', 'terror', 'friendly justice'];
+        /////// DELETE WHEN ACTIVE /////
+    }
+    
+});
