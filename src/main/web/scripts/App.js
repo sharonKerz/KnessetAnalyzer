55b33c0f33e4438c7707bd5409abe5cb42074ec2
Guy Ben Moshe <gbt3101@gmail.com>
2016-08-04 19:50:12 +0300

    improvements

--- src/main/web/scripts/App.js
+++ src/main/web/scripts/App.js
@@ -1,5 +1,5 @@
 'use strict';
-var app = angular.module("knessetAnalyzer", []); 
+var app = angular.module("knessetAnalyzer", ["ng-fusioncharts"]); 
 app.controller("mainCtrl", function($scope, $http) {
     $scope.dates = {startDate: new Date(99), endDate: new Date()};
     //$scope.startDate = new Date(99);
@@ -8,6 +8,32 @@ app.controller("mainCtrl", function($scope, $http) {
     $scope.sendingAddress = 'localhost';
     $scope.received = false;
     $scope.sent = false;
+    //$scope.type = "bar3d"; // Types: bar3d, pie3d, column3d, 
+    $scope.type = "column3d";
+    $scope.types = {
+        pie: false,
+        bar: true,
+        column: false,
+        line: false
+    }
+    
+    $scope.myDataSource = {
+                chart: {
+                    caption: "Subjects Discussed",
+                    subCaption: "Top Subjects between "+$scope.dates.startDate.toDateString()+" and "+$scope.dates.endDate.toDateString(),
+                    /*startingangle: "120",
+                    showlabels: "0",
+                    showlegend: "1",
+                    enablemultislicing: "0",
+                    slicingdistance: "15",
+                    showpercentvalues: "1",
+                    showpercentintooltip: "0",
+                    plottooltext: "Age group : $label Total visit : $datavalue",
+                    theme: "fint"*/
+                },
+                data:
+                [ ]
+              };
     
     $scope.submit = function() {
         console.log("Submit Activated");
@@ -16,7 +42,26 @@ app.controller("mainCtrl", function($scope, $http) {
         // FOR DEBUG ONLY:
         $scope.sent=false;
         $scope.received = true;
-        $scope.subjectList = ['Security', 'Economy', 'Stupid Muslims', 'terror', 'friendly justice'];
+        $scope.myDataSource.data = [{
+                    label: "Security",
+                    value: "880000"
+                },
+                {
+                    label: "Economy",
+                    value: "730000"
+                },
+                {
+                    label: "Stupid Muslims",
+                    value: "590000"
+                },
+                {
+                    label: "Terror",
+                    value: "520000"
+                },
+                {
+                    label: "Friendly Justice",
+                    value: "330000"
+                }];
         /////// DELETE WHEN ACTIVE /////
         
         $http.post("http://"+$scope.sendingAddress+":3000/datesQuery", 
@@ -24,9 +69,24 @@ app.controller("mainCtrl", function($scope, $http) {
             .success(function(response) {
                 $scope.sent = false;
                 $scope.received = true;
-                $scope.subjectList = response;
+                $scope.myDataSource.data = response;
             });
         
+    }
+    
+    
+    
+    $scope.pieType = function() {
+        $scope.type="pie3d";
+    }
+    $scope.columnType = function() {
+        $scope.type="column3d";
+    }
+    $scope.barType = function() {
+        $scope.type="bar3d";
+    }
+    $scope.lineType = function() {
+        $scope.type="line";
     }
     
 });
